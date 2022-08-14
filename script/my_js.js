f2cd541c96068af203322386bc269b4438522751
yevstrikha <yevstrikha@gmail.om>
2022-08-14 12:57:38 -0400

    done owl-carousel

--- script/my_js.js
+++ script/my_js.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+})
